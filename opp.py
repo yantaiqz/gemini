ef7d3e8603e1c8d00a0b1f68582cb2c17bf84e3e
yantaiqz <yantaiqz@googlemail.com>
2025-12-06 10:38:41 +0800

    Update opp.py

--- opp.py
+++ opp.py
@@ -340,7 +340,8 @@ def initialize_model(lang_code):
     }
     sys_instruction = get_system_instruction(lang_code)
     model = genai.GenerativeModel(
-        model_name='gemini-2.0-flash', 
+        #model_name='gemini-2.0-flash', 
+        model_name='gemini-2.0-pro', 
         system_instruction=sys_instruction,
         generation_config=generation_config
     )
